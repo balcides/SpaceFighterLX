6dce4901aa9414526ffa47558ee84405ecd62c9d
gabe <balcides@gmail.com>
2014-06-29 19:50:48 -0700

    Lesson 12 - invented a new array/class to assign
    found a way to add strings to a class. Damn, wish somehow this was
    easier but I found a way, its just not ideal. ;(

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -21,32 +21,34 @@ public class Score : MonoBehaviour {
 	public GUIStyle textGUIStyle;
 	public float heightOffset = 50;
 
-	public string[] highscoreName = new string[10];
-
+	public Scores scores = new Scores();
+	public Highscore[] score = new Highscore[10];
 
 
 	void Awake(){
 
 		center = GetComponent<_GUIClasses>();
-
-		highscoreName[0] = "Juan";
-		highscoreName[1] = "Kelper";
-		highscoreName[2] = "Harper";
-		highscoreName[3] = "Don";
-		highscoreName[4] = "Cisco Kid";
-		highscoreName[5] = "Unitee";
-		highscoreName[6] = "Gebee";
-		highscoreName[7] = "Gilbert";
-		highscoreName[8] = "Hopper";
-		highscoreName[9] = "Wilfredo";
-
 		boxStartLocation = new Vector2(-300,-200);
 		heightOffset = 50;
+
+		for( int i = 0; i < 10; i++){ score[i] = new Highscore(); }
+
+		score[0].name = "Donny";
+		score[1].name = "Kelper";
+		score[2].name = "Harper";
+		score[3].name = "Don";
+		score[4].name = "Cisco Kid";
+		score[5].name = "Unitee";
+		score[6].name = "Gebee";
+		score[7].name = "Gilbert";
+		score[8].name = "Hopper";
+		score[9].name = "Wilfredo";
 
 	}
 	
 	// Use this for initialization
 	void Start () {
+
 
 	}
 	
@@ -54,26 +56,30 @@ public class Score : MonoBehaviour {
 	void Update () {
 		center.location.updateLocation();
 	}
-	
-	public class Scores
-	{
-		public void initialize()
-		{
-			print("Score System initialized");
-		}
-		
-	}
-	
-	public Scores scores = new Scores();
-	
+
 
 	void OnGUI() {
 
 		for( int i = 0; i < 9; i++){
 			GUI.Box(new Rect(center.location.offset.x + boxStartLocation.x, 
 			                 center.location.offset.y + boxStartLocation.y + i * heightOffset, 120, 30),
-			        		 highscoreName[i], textGUIStyle);
+			        		 score[i].name, textGUIStyle);
 		}
+	}
+
+	public class Scores{
+		public void initialize(){
+			print("Score System initialized");
+		}
+	}
+
+	public class Highscore{
+		public string name { get; set; }
+		//public string name;
+		public int rounds;
+		public int kills;
+
+		public Highscore(){}
 	}
 	
 }
